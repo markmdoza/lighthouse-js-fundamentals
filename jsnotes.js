d8f352a233bc4d9be56ac88e29de6de34a5331bc
Mark Mendoza <marklmdoza@gmail.com>
2023-06-02 15:05:40 -0400

    Finished functions on Udacity

--- jsnotes.js
+++ jsnotes.js
@@ -160,9 +160,172 @@ console.log(laugh());
 /* Laugh It Off Quiz 2 */
 
 function laugh(num); {
-  
+  for (let num = 1; num <=3; num++)
+  console.log(num + "!")
+}
+console.log(laugh(3));
+
+/*********************************************/
+
+/* Build A Triangle (5-3) */ 
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a `buildTriangle()` function
+ * - Your `buildTriangle()` function should take one argument (or you can say parameter)
+ * - Your `laugh()` function should build the triangle as describe above
+ */
+
+// creates a line of * for a given length
+
+function makeLine(length) {
+    var line = "";
+    for (var j = 1; j <= length; j++) {
+        line += "* ";
+    }
+    return line + "\n";
 }
 
+// your code goes here.  Make sure you call makeLine() in your own code.
+
+function buildTriangle(x) {
+  let triangle = "";
+  for (t = 1; t <= x; t++) {
+    triangle += makeLine(t);
+  }
+  return triangle;
+}
+// test your code by uncommenting the following line
+console.log(buildTriangle(10));
+
+/*** SOLUTION ***/
+ 
+// creates a line of * for a given length
+function makeLine(length) {
+    var line = "";
+    for (var j = 1; j <= length; j++) {
+        line += "* ";
+    }
+    return line + "\n";
+}
+
+// your code goes here.  Make sure you call makeLine() in your own code.
+function buildTriangle(length) {
+    // Let's build a huge string equivalent to the triangle
+    var triangle = "";
+    
+    //Let's start from the topmost line
+    var lineNumber = 1;
+    
+    for(lineNumber=1; lineNumber<=length; lineNumber++){
+        // We will not print one line at a time.
+        // Rather, we will make a huge string that will comprise the whole triangle
+        triangle = triangle + makeLine(lineNumber);
+    }
+    return triangle;
+}
+
+// test your code by uncommenting the following line
+// Note that the function buildTriangle() must return a string 
+// because the console.log() accepts a string argument
+console.log(buildTriangle(10));
 
 
-console.log(laugh(3));
+/* Function Expression Recap */
+What you've learned so far:
+Function Expression: When a function is assigned to a variable. The function can be named, or anonymous. Use the variable name to call a function defined in a function expression.
+
+// anonymous function expression
+var doSomething = function(y) {
+  return y + 1;
+};
+// named function expression
+var doSomething = function addOne(y) {
+  return y + 1;
+};
+// for either of the definitions above, call the function like this:
+doSomething(5);
+Returns: 6
+
+You can even pass a function into another function inline. This pattern is commonly used in JavaScript, and can be helpful streamlining your code.
+
+// function declaration that takes in two arguments: a function for displaying
+// a message, along with a name of a movie
+function movies(messageFunction, name) {
+  messageFunction(name);
+}
+
+// call the movies function, pass in the function and name of movie
+movies(function displayFavorite(movieName) {
+  console.log("My favorite movie is " + movieName);
+}, "Finding Nemo");
+
+
+/*** Quiz: Laugh (5-4) ***/
+
+/* SOLUTION */
+let laugh = funtion(num) {
+  let laughString = "";
+
+  for (i = 0; i < num; i++) {
+    laughString += "ha"
+  }
+  laughString += "!";
+  return laughString;
+}
+
+console.log(laugh(10));  
+
+/*** Quiz: Cry (5-5) ***/
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a variable `cry`
+ * - Your code should include a named function expression stored in the variable `cry`
+ * - Your code should call the function expression using the variable name, not the function name
+ * - Your function expression should return the expected output
+ * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+ */
+
+
+// your code goes here
+
+let cry = function makeCry(n) {
+  let cryString = "boohoo!";
+  return cryString
+}
+
+console.log(cry);
+
+ * Programming Quiz: Inline Functions (5-6)
+ */
+ 
+ /*
+ * QUIZ REQUIREMENTS
+ * - Your code should have an `emotions()` function
+ * - Your code should call the `emotions()` function
+ * - Your `emotions()` function call should have an inline function expression passed as the second parameter
+ * - Your function expression should return the expected output
+ */
+
+
+// don't change this code
+// emotions() function definition
+function emotions(myString, myFunc) {
+    console.log("I am " + myString + ", " + myFunc(2));
+}
+
+emotions("happy", function laugh(num) {
+    let laughString = "";
+    
+    for (i = 0; i < num; i++) {
+        laughString += "ha";
+    }
+    laughString += "!";
+    return laughString;
+});
+
+// your code goes here
+// Call the emotions() function with two arguments
+// Argument 1 - "happy" string
+// Argument 2 - an inline function expression 
